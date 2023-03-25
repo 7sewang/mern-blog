72d6ebd6c1aaf879989cc79404860ae5f9eb0509
7sewang <dev.7sering@gmail.com>
2023-03-25 09:25:26 +0545

    Frontend UI complete

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -12,6 +12,46 @@ export function App() {
           <a href="">Regiser</a>
         </nav>
       </header>
+        <div className="post">
+    <div className="image">
+    <img src="https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png" alt="" srcset="" />
+
+    </div>
+            <div className="texts">
+          <h2>How Become a fullstack MERN developer in 2023</h2>
+          <p className="info">
+            <a className="author">Kenzo Ryan</a>
+            <time>2023-05-26 02:36</time>
+          </p>
+          <p className="summery">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad rerum numquam ipsam adipisci cum voluptas unde neque repudiandae, nobis voluptate, molestiae beatae, dolorum itaque vero repellendus excepturi facere sunt deserunt.</p>
+          </div>
+        </div>
+        <div className="post">
+         <div className="image">
+         <img src="https://static.wixstatic.com/media/0e0314_0a37b8770f5342f58fbad16bf83bc330~mv2.png/v1/fill/w_640,h_366,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/0e0314_0a37b8770f5342f58fbad16bf83bc330~mv2.png" alt="" srcset="" />
+         </div>
+         <div className="texts">
+          <h2>How Become a fullstack MERN developer in 2023</h2>
+          <p className="info">
+            <a className="author">Kenzo Ryan</a>
+            <time>2023-05-26 02:36</time>
+          </p>
+          <p className="summery">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad rerum numquam ipsam adipisci cum voluptas unde neque repudiandae, nobis voluptate, molestiae beatae, dolorum itaque vero repellendus excepturi facere sunt deserunt.</p>
+          </div>
+        </div>
+        <div className="post">
+         <div className="image">
+         <img src="https://static.wixstatic.com/media/c7e19c_ff4f9746a58c45529ac929f0f9aa8fb5~mv2.jpg/v1/fill/w_772,h_451,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c7e19c_ff4f9746a58c45529ac929f0f9aa8fb5~mv2.jpg" alt="" srcset="" />
+         </div>
+         <div className="texts">
+          <h2>How Become a fullstack MERN developer in 2023</h2>
+          <p className="info">
+            <a className="author">Kenzo Ryan</a>
+            <time>2023-05-26 02:36</time>
+          </p>
+          <p className="summery">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad rerum numquam ipsam adipisci cum voluptas unde neque repudiandae, nobis voluptate, molestiae beatae, dolorum itaque vero repellendus excepturi facere sunt deserunt.</p>
+          </div>
+        </div>
      </main>
     </>
   )
