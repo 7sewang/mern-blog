2006d21f8dbeaeb13c40a3c6cf62e22fc6c03d0d
7sewang <dev.7sering@gmail.com>
2023-03-25 15:06:34 +0545

    registraion new user complet

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
-  return (
-  <>
-   <form className="register" >
-      <h1>Register</h1>
-      <input type="text"
-             placeholder="username"
-          />
-      <input type="password"
-             placeholder="password"
-           />
-      <button>Register</button>
-    </form>
-  </>
-  )
-}
+  const [register, setRegister] = useState([]);
 
-export default Register
+  const newUser = async (register) => {
+    try {
+      const response = await axios.post("http://localhost:8000/register", register);
+      if (response.status === 200) {
+        alert('registration successful');
+      } else {
+        alert('registration failed');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('an error occurred during registration');
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    newUser(register);
+  };
+
+  const chanegeHanle = (e) => {
+    setRegister({
+      ...register,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <>
+      <form className="register">
+        <h1>Register</h1>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={chanegeHanle}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={chanegeHanle}
+        />
+        <button onClick={handleSubmit}>Register</button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
