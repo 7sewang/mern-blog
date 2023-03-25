1180bd2cbfd7ec84813db73af3602616a5a45613
7sewang <dev.7sering@gmail.com>
2023-03-25 08:37:14 +0545

    initial commit

---
+++ client/src/app.jsx
@@ -0,0 +1,19 @@
+
+
+export function App() {
+
+  return (
+    <>
+     <main>
+      <header>
+        <a href="" className="logo"> My Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Regiser</a>
+        </nav>
+      </header>
+     </main>
+    </>
+  )
+}
+ 
