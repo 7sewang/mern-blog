018c49e2b397c233d9441a9924388c1c3e599710
7sewang <dev.7sering@gmail.com>
2023-03-25 22:29:48 +0545

    Login functionlity complete

--- api/index.js
+++ api/index.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 const server = express();
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+const jwt = require("jsonwebtoken")
 
-const port = process.env.PORT || 5000;
-server.use(cors());
+const port = process.env.PORT || 8000;
 server.use(express.json()); // middleware for body json
+server.use(cors({credentials:true,origin:'http://127.0.0.1:5173'}));
 
 main().catch((err) => console.log(err));
 
@@ -16,14 +20,38 @@ async function main() {
   console.log("Database connected successfully...");
 }
 
+// Register
 server.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const userDoc = await User.create({ username, password });
-        res.json(userDoc);
-    } catch (error) {
-      res.status(400).json(error)
-    }
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// Login 
+
+server.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id:userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json('wrong credentials');
+  }
 });
 
 server.listen(port, () => {
