1d074ec6627c9f74afc5373b95576c4e2033bac7
7sewang <dev.7sering@gmail.com>
2023-03-26 19:24:37 +0545

    login and logout function done

--- api/index.js
+++ api/index.js
@@ -6,12 +6,16 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const salt = bcrypt.genSaltSync(10);
-const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-const jwt = require("jsonwebtoken")
+const secret = "asdfe45we45w345wegw345werjktjwertkj";
+const jwt = require("jsonwebtoken");
+
+const cookieParser = require("cookie-parser");
+
+server.use(cookieParser());
 
 const port = process.env.PORT || 8000;
 server.use(express.json()); // middleware for body json
-server.use(cors({credentials:true,origin:'http://127.0.0.1:5173'}));
+server.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" }));
 
 main().catch((err) => console.log(err));
 
@@ -34,7 +38,7 @@ server.post("/register", async (req, res) => {
   }
 });
 
-// Login 
+// Login
 
 server.post('/login', async (req,res) => {
   const {username,password} = req.body;
@@ -53,7 +57,20 @@ server.post('/login', async (req,res) => {
     res.status(400).json('wrong credentials');
   }
 });
+// Profile endpoint that requires a valid JWT token
+server.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
 
+server.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+
+// Listen
 server.listen(port, () => {
   console.log(`Server started on ${port} port successfully..`);
 });
